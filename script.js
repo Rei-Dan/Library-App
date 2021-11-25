dd12353c56718c3def03407a700a32af9b502de0
Nick van Ekeren <nvekeren@gmail.com>
2021-11-25 15:46:12 +0100

    added button to toggle read status, added some css styling

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ book.prototype.info = function () {
   );
 };
 
+book.prototype.readstatus = function () {
+  if (this.read === "Read") {
+    this.read = "Not Read";
+  } else {
+    this.read = "Read";
+  }
+};
+
+const bookshowcase = document.querySelector(".bookshowcase");
+let deletebuttons = document.querySelectorAll(".delete-button");
+let togglebuttons = document.querySelectorAll(".read-status");
+
 // ##Declare empty library
 let myLibrary = [];
 // {
@@ -21,7 +33,6 @@ let myLibrary = [];
 //   read: "Read",
 // },
 // ##Select all buttons on the pages that have the class of .delete-button
-let deletebuttons = document.querySelectorAll(".delete-button");
 
 // function to grab all the values from the input fields, pushes these as
 // an object to the myLibrary Array and invokes the addbook() function
@@ -34,35 +45,29 @@ function addBookToLibrary() {
   addbook();
 }
 
-// function deletebook() {
-//   const booktarget = deletebutton.closest(".book");
+// function addDeletefunction() {
+//   deletebuttons = document.querySelectorAll(".delete-button");
+//   deletebuttons.forEach((deletebutton) => {
+//     deletebutton.addEventListener("click", () => {
+//       let booktarget = deletebutton.parentNode.parentNode;
+//       // const booktarget = deletebutton.closest("[data-book-number]");
+//       // console.log(booktarget);
+//       // console.log(booktarget.dataset.bookNumber);
+//       myLibrary.splice(booktarget.dataset.bookNumber - 1, 1);
+//       bookshowcase.removeChild(booktarget);
+//       reorderBooks(booktarget.dataset.bookNumber);
+//     });
+//   });
 // }
 
-function addDeletefunction() {
-  deletebuttons = document.querySelectorAll(".delete-button");
-  deletebuttons.forEach((deletebutton) => {
-    deletebutton.addEventListener("click", () => {
-      const booktarget = deletebutton.parentNode.parentNode;
-      // const booktarget = deletebutton.closest("[data-book-number]");
-      // console.log(booktarget);
-      // console.log(booktarget.dataset.bookNumber);
-      myLibrary.splice(booktarget.dataset.bookNumber - 1, 1);
-      bookshowcase.removeChild(booktarget);
-      reorderBooks(booktarget.dataset.bookNumber);
-    });
-  });
-}
-
 function reorderBooks(booktarget) {
-  const books = bookshowcase.querySelectorAll(".book");
+  let books = bookshowcase.querySelectorAll(".book");
   books.forEach((book) => {
     if (book.dataset.bookNumber > booktarget) {
       book.dataset.bookNumber -= 1;
     }
   });
 }
-
-const bookshowcase = document.querySelector(".bookshowcase");
 
 function addbook() {
   const book = document.createElement("div");
@@ -73,8 +78,15 @@ function addbook() {
   const bookheader = document.createElement("div");
   book.appendChild(bookheader);
   bookheader.classList.add("bookheader");
+  //# add deletebutton
   const deletebutton = document.createElement("button");
   deletebutton.innerHTML = "&times";
+  deletebutton.addEventListener("click", () => {
+    const booktarget = deletebutton.parentNode.parentNode;
+    myLibrary.splice(booktarget.dataset.bookNumber - 1, 1);
+    bookshowcase.removeChild(booktarget);
+    reorderBooks(booktarget.dataset.bookNumber);
+  });
   deletebutton.classList.add("delete-button");
   bookheader.appendChild(deletebutton);
   const name = document.createElement("div");
@@ -86,11 +98,23 @@ function addbook() {
   book.appendChild(pages);
   const read = document.createElement("div");
   book.appendChild(read);
+  read.classList.add("read-status");
+  const readdiv = document.createElement("div");
+  book.appendChild(readdiv);
+  readdiv.classList.add("readdiv");
+  const readbutton = document.createElement("button");
+  readdiv.appendChild(readbutton);
+  readbutton.addEventListener("click", () => {
+    myLibrary[book.dataset.bookNumber - 1].readstatus();
+    read.textContent = `Status: ${myLibrary[book.dataset.bookNumber - 1].read}`;
+  });
+  readbutton.innerHTML = "Change Read Status";
+  readbutton.classList.add("toggle-read");
   name.textContent = myLibrary[myLibrary.length - 1].title;
   author.textContent = myLibrary[myLibrary.length - 1].author;
   pages.textContent = `${myLibrary[myLibrary.length - 1].pages} pages`;
-  read.textContent = `Read: ${myLibrary[myLibrary.length - 1].read}`;
-  addDeletefunction();
+  read.textContent = `Status: ${myLibrary[myLibrary.length - 1].read}`;
+  // addDeletefunction();
 }
 // addbook();
 
